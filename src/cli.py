593628ca82e9cc8e64e70a18da884de925e2c56c
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-10-02 17:59:33 +0100

    Initial commit.

---
+++ src/cli.py
@@ -0,0 +1,7 @@
+# CLI handling logic for the application and entry point for user interaction.
+
+# /-- Program psuedocode --/
+# 1. Display welcome message, current date/time, and menu of options.
+# 2. If creating a note, prompt for title, content, and importance, and then create an entry in SQLite DB.
+# 3. If viewing notes, fetch and display all notes from SQLite DB.
+
---
+++ src/core.py
@@ -0,0 +1,11 @@
+# Core logic for the application.
+
+# Represents a note.
+class Note:
+    def __init__(self, title, content, is_important):
+        self.title = title
+        self.content = content
+        self.is_important = is_important
+
+    
+    
